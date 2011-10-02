15681b75bf7f91e758257148957df09cf5b994ea
Jud Porter <judson.porter@gmail.com>
2011-10-02 13:30:18 -0400

    Add simple installation script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Initialize submodules
+git pull
+git submodule init
+git submodule update
+
+# Create a backup directory
+backup_dir=backup-$(date +"%Y-%m-%d_%Hh%Mm%Ss")
+mkdir -p $backup_dir;
+
+for f in bashrc cshrc gvimrc screenrc vim vimrc; do
+    new=$(pwd)/$f
+    old=~/.$f
+
+    # If file already exists, create a backup
+    if [[ -e $old ]]; then
+        mv $old $backup_dir;
+    fi;
+
+    # Create the symlink
+    ln -sf $new $old;
+done;

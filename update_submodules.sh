53c41d88daadc411f8416ff9e97ad31ed4a0e0df
Jud Porter <judson.porter@gmail.com>
2012-03-27 12:23:43 -0400

    Use git submodule foreach to update submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/env sh
-SUBMODULES=`git submodule | awk '{print $2}'`
-TOPDIR=$PWD
-
-for submodule in $SUBMODULES
-do
-    echo $submodule
-    cd $submodule
-    git checkout master
-    git pull
-    cd $TOPDIR
-    echo
-done 
+git submodule foreach 'git checkout master; git pull'
